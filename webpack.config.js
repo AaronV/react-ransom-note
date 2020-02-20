f19734e0f3890f800779ec544841f0faf07ef19b
Aaron Vanderpoel <aaron.vanderpoel@gmail.com>
2020-02-19 20:57:04 -0500

    config is the worst

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('lib'),
-    filename: 'ReactRansomNote.js',
+    filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
   module: {
